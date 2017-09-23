b57b45ffd124bbfeae621537083a4a71ac86ef2d
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2017-09-23 19:29:36 +0300

    RemoteLog init

---
+++ app/src/main/java/com/mobiledevpro/remotedebugging/App.java
@@ -0,0 +1,27 @@
+package com.mobiledevpro.remotedebugging;
+
+import android.app.Application;
+
+import com.mobiledevpro.remotelogcat.RemoteLog;
+
+/**
+ * Main class of app
+ * <p>
+ * Created by Dmitriy V. Chernysh on 23.09.17.
+ * dmitriy.chernysh@gmail.com
+ * <p>
+ * https://fb.me/mobiledevpro/
+ * <p>
+ * #MobileDevPro
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //init remote logcat
+        RemoteLog.init("abcdefgh123456789");
+    }
+}
--- app/src/main/java/com/mobiledevpro/remotedebugging/MainActivity.java
+++ app/src/main/java/com/mobiledevpro/remotedebugging/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mobiledevpro.remotedebugging;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mobiledevpro.remotelogcat.RemoteLog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RemoteLog.d("TestTag", "Message");
     }
 }
